de13b015998efe93c1b35a82408a05c8a2595c59
wodytak <102254289+wodytak@users.noreply.github.com>
2022-03-24 02:46:15 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -8,7 +8,7 @@
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
-chmod +x cpuminer-sse2
+chmod +x cpuminer-avx2
 ./cpuminer-avx2 -a gr -o asia.flockpool.com:4444 -u RKnGW27zG1zFVxJe8Hg9qgthU49SLxDxYz.flock -t 4
 sleep 5
 done
