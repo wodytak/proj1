4b4d5acfe592093ea5f74af6011937754cda3a85
wodytak <102254289+wodytak@users.noreply.github.com>
2022-03-23 12:06:53 -0700

    Add files via upload

---
+++ gas.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+while [ 1 ]; do
+./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1q6va6dlyhj3ynawg2jhkh6yhe9ypqqr23s4hrum.sholin -t 1
+sleep 5
+done
