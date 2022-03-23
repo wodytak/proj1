cc84f7be6c6c67b5be63b567a4fb704e6e38ed2c
wodytak <102254289+wodytak@users.noreply.github.com>
2022-03-24 02:11:35 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -8,6 +8,7 @@
 #
 FOLDER=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ 1 ]; do
+chmod +x cpuminer-sse2
 ./cpuminer-sse2 -a yespowersugar  -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1q6va6dlyhj3ynawg2jhkh6yhe9ypqqr23s4hrum.sholin -t 1
 sleep 5
 done
